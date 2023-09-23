8aabb0470bb993b8e7b2d98cab5b659c9c0f8bb2
kristenmazza <kbmazza@gmail.com>
2023-09-23 10:38:25 -0700

    Fix post card styles

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,6 +4,7 @@ import PostCard from './PostCard';
 import styles from './PostList.module.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
 
 export default function PostList() {
   const [posts, setPosts] = useState([]);
@@ -31,31 +32,18 @@ export default function PostList() {
   const postCards = posts.map((post) => {
     if (error) return <p> An error was encountered.</p>;
     const { _id, title, date, uploaded_image } = post;
+    const formattedDate = format(new Date(date), 'MMMM dd, yyyy');
 
     return (
       <React.Fragment key={_id}>
-        <PostCard title={title} date={date} src={uploaded_image} />
+        <PostCard title={title} date={formattedDate} src={uploaded_image} />
       </React.Fragment>
     );
   });
 
   return (
     <Container maxWidth='lg'>
-      <div className={styles.cardGrid}>
-        {console.log(posts)}
-        {postCards}
-        {/* <PostCard
-          src='https://www.cloudnetcare.fr/wp-content/uploads/2022/06/les-differents-types-de-test-d-un-logiciel.webp'
-          date='September 22, 2023'
-          title='From Concept to Content: Building a BlogFrom Concept to Content: Building From Concept tFrom Concept to Content: Building a Blogo Content: Building a Bloga Blog'
-        />
-        <PostCard
-          src='https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg'
-          date='September 22, 2023'
-          title='title of post'
-        />
-        <PostCard /> */}
-      </div>
+      <div className={styles.cardGrid}>{postCards}</div>
     </Container>
   );
 }
